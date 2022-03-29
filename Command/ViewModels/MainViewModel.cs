c2de2beb9df1b49c9539676f958981b442135a59
Vít Zeman <vit.zem@seznam.cz>
2022-03-29 21:43:08 +0200

    Přidejte soubory projektu.

---
+++ Command/ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private ObservableCollection<char> _usedChars;
+        private string _originalWord = "Karel";
+        public string _hiddenWord = "_____";
+        public ParametrizedRelayCommand<string> GuessCharacterCommand { get; set; }
+
+        public MainViewModel()
+        {
+            _usedChars = new ObservableCollection<char> {};
+            GuessCharacterCommand = new ParametrizedRelayCommand<string>(
+                (value) =>
+                {
+                    if (!String.IsNullOrEmpty(value) && !_usedChars.Contains(value[0]))
+                    {
+                        _usedChars.Add(value[0]);
+                        GuessCharacterCommand.RaiseCanExecureChanged();
+                    }
+                },
+                
+                (p) => { 
+                    return p == null ? true : !_usedChars.Contains(p[0]); }
+                );
+        }
+
+        public string Word
+        {
+            get
+            {
+                return _hiddenWord;
+            }
+            set
+            {
+                _hiddenWord = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
